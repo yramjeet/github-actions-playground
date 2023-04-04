3979423769bcf2d9ef86386d2ee67086c3a3d5c3
yramjeet <ramjeet.yadav@peak.ai>
2023-04-04 14:19:49 +0530

    feat: setup

---
+++ apps/app-a/src/add.js
@@ -0,0 +1,17 @@
+function adderFive(num){
+    return num + 5;
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function adderTwo(num){
+    return num + 2;
+}
+
+module.exports = {
+    add,
+    adderFive,
+    adderTwo
+}
---
+++ apps/app-b/src/sub.js
@@ -0,0 +1,17 @@
+function subFive(num){
+    return num - 5;
+}
+
+function sub(num1, num2){
+    return num1 - num2;
+}
+
+function subTwo(num){
+    return num - 2;
+}
+
+module.exports = {
+    sub,
+    subFive,
+    subTwo
+}
